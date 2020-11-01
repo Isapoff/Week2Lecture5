a1d21e60c967b2af490399a0cedb64934b572ad9
isapoff <iskender.saparov.is@gmail.com>
2020-11-01 22:48:24 +0600

    initial commit

---
+++ lecture4.py
@@ -0,0 +1,108 @@
+xz# dict = {"key:" "value"}
+
+# Bishkek = {
+#      Univery:{
+#          Gruppy:{
+#              studenty:{
+
+#              }
+#          }
+#      }
+# }
+
+# student = {
+#     'name': 'Bakyt', 
+#     'surname': 'Bakytov', 
+#     'age': 23,
+#     'languages':['english', 'russian', 'kyrgyz'],
+#     'smokes': False,
+#     'other_dict': {1:'one', 2:'two'},
+#     True: "True goi",
+#     None: 'None goi',
+#     (1, 2, 3): "eto tuple",
+#     }
+
+# student.cleastudent[(1, 2, 3)])
+# print(student[None])
+# print(student[True])
+# print(student['other_dict'][2])r()
+# new_student  = student.copy()
+# student.clear()
+
+# print(student.items())
+# for item in student.items():
+# print(student.keys())
+# for item in student.keys():
+# print(student.values())
+# for item in student.values():
+#     print(item)
+
+# new_item = student.pop('name', 'Nurbek')
+# print(new_item)
+# print(student)
+
+# new_item = student.popitem()
+# student.setdefault(10, "DEFAULT")
+# student.update([('black', 6), ('white', 8)])
+# print(new_item)
+# student.update({'key1':1, 'key2': 2})
+# print(student)
+
+
+# print(student[(1, 2, 3)])
+# print(student[None])
+# print(student[True])
+# print(student['other_dict'][2])
+# studenty = {}
+# print(type(studenty))
+# student = dict((("witer", 1), ("spring", 2), ("fall", 3)))
+# print(student)
+# student['name'] = "Askat"
+# print(student['name'])
+# del student['name']
+# print(student)
+
+# surname = student.get('sur_name')
+# name = student['username']
+# print(surname)
+# print(name)
+
+# my_tuple = [("one", 1), ("two", 2), ("three", 3)]
+# dict_ = dict(my_tuple)
+# print(dict_)
+
+# days = [1, 2, 3]
+# days_names = ['mon', 'thus', 'wed']
+# one_more = [5, 6, 7]
+# dict_days = zip(days, days_names, one_more)
+# print(dict_days)
+
+# dict()
+
+
+# student = dict.fromkeys([1, 2, 3], 'Makers')
+# print(student)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
