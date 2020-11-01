033417afdb7e90ee722c576ada2396699000d661
isapoff <iskender.saparov.is@gmail.com>
2020-11-01 17:56:58 +0600

    initial commit

---
+++ lecture1.py
@@ -0,0 +1,57 @@
+# name = input("Enter your name: ")
+# print(type(name))
+# name = name.lower()
+# name = name.upper()
+# name = name.capitalize()
+# name = name.title()
+# print(name)
+
+# some_string = "Hello world"
+# print(some_string.find('l'))
+# print(some_string.find('a'))
+# print(some_string.rfind('l'))
+# print(some_string.index('o'))
+# print(some_string.count('l'))
+# print(dir(''))
+
+# some_str = input("Enter some string: " ).strip()
+
+
+# some_str = input("Enter some string: " ).replace('a', 'o')
+# some_str = input("Enter some string: " ).split('-')
+# some_str = input("Enter some string: " ).upper().split(' ')
+# print(some_str)
+
+# name = "Isken234"
+# print(dir(""))
+# print(name.isalpha())
+# name = "12345"
+# print(name.isdigit())
+# name = "\tIsken \nDavlet"
+# print(name.rstrip('', ''))
+
+
+# name = input('Enter your name: ')
+# age = 20
+# print('Hello ' + name + '. Your age is ' + age) - nelzya
+# print('Hello', name, 'Your age is', age)
+# print(f'Hello, {name}. Your age is {age}.')
+# print('Hello, {}. Your age is {}.'.format(name, age))
+
+
+
+# import math
+# a = math.pi
+# print(a)
+
+
+# def is_even(number):
+#     return number % 2 == 0
+# def test_is_even():
+#     assert is_even(2) == True
+#     assert is_even(3) == False
+#     assert is_even(8) == True
+#     assert is_even(100) == True
+#     assert is_even(101) == False
+#     print("YOUR CODE IS CORRECT!")
+# test_is_even()
