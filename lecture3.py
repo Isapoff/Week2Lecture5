b88733abfb1f7c6f464e862e9f023356762b3924
isapoff <iskender.saparov.is@gmail.com>
2020-11-01 22:43:02 +0600

    initial commit

---
+++ lecture3.py
@@ -0,0 +1,106 @@
+# list_ = list()             sozdanie spiska
+# list_ = []                 sozdanie spiska
+# list_ = list('hello')
+# print(type(list_))
+# print(list_)
+
+
+# a = 'hello'
+# b = a.replace('l', 'k')
+# print(b)
+
+
+# a = [1,2,3]
+# a.pop()
+# print(a)
+# a = [1,2,3,4, [1,3,4]]
+# print(len(a))
+# a[1] = 5
+# a.insert(0, 0)
+# print(a)
+
+# a = [1, 2, 3, 4, 5, 6]
+# b = ['a', 'b']
+# c = a + b
+# a = a * 3                dublirovanie *
+# a[0] = a[0]*3
+
+# a.extend(b)
+# print(a)
+
+# a = a[2::2]
+# print(a)
+
+# del a
+# print(a)
+
+# a = ()
+# b = [1, 2, 3]
+# a = tuple('hello')
+# a = tuple(b)
+# c = (1,)                  chtoby stalo tuple nado posle znachenia postavit zapyatyu
+# print(type(c))
+# print(a)                    dir vytaskivaet kakie comandy nado ispolzovat
+
+# a = ()
+# print(dir(a))
+
+# a = ('a', 1, 2, False, None, [1, 2], {'key': 'value'})
+# print(a.count('a'))
+# print(a.index([1, 2]))
+# a[5][0] = 5
+# print(a)
+
+# a = 2
+# b = 3
+# a,b = b,a
+# c = a
+# a = b
+# b = c
+# print('a', a)
+# print('b', b)
+
+# a = 1, 'a'
+# print(type(a))
+
+# a = (1, 2, 3)        unpacking
+# b, c, d = a          * zabiraet ostavshiesya cifry
+# print(a)
+# print(b)
+# print(c)
+# print(d)
+
+# a, b = 2, 3
+# print(a)
+# print(b)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
